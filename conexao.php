8ff854c18c35ea7329795442a84e5eae1d03a618
AlanNs12 <92896200+AlanNs12@users.noreply.github.com>
2025-05-19 11:23:07 -0300

    banco-remoto
    adição da conexão do banco de dados remoto com proteção para as credenciais do arquivo

--- conexao.php
+++ conexao.php
@@ -1,13 +1,22 @@
 <?php
-$host = 'localhost';
-$db = 'controle_acesso';
-$user = 'root';
-$pass = '';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
 }
-?>
